fd42519b0901b017e39f1ceb83414629aca718c6
Satyuel <stockmann89@mail.ru>
2023-11-23 01:14:32 +0300

    Implement user auto registration

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -1,9 +1,13 @@
+import logging
+
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from rest_framework.authentication import BaseAuthentication
 from django.conf import settings
-from aiogram.utils.web_app import safe_parse_webapp_init_data
+from aiogram.utils.web_app import safe_parse_webapp_init_data, WebAppInitData
 
-User = get_user_model()
+logger = logging.getLogger(__name__)
+User: AbstractUser = get_user_model()
 
 
 class WebAppAuthentication(BaseAuthentication):
@@ -18,14 +22,29 @@ class WebAppAuthentication(BaseAuthentication):
             raise ValueError("Invalid token format")
         return auth_info[1]
 
+    @staticmethod
+    def get_user(user_data: WebAppInitData) -> User:
+        user, _ = User.objects.get_or_create(
+            telegram_chat_id=user_data.user.id,
+            defaults={
+                "username": "TG:" + str(user_data.user.id),
+                "first_name": user_data.user.first_name,
+                "last_name": user_data.user.last_name or "",
+            },
+        )
+        return user
+
     def authenticate(self, request):
         try:
             token = self.parse_token(request)
             user_data = safe_parse_webapp_init_data(settings.BOT_TOKEN, token)
-            telegram_chat_id = user_data.user.id
-            user = User.objects.get(
-                telegram_chat_id=telegram_chat_id, telegram_chat_id__isnull=False
-            )
+            if user_data.user is None:
+                raise ValueError("Token has no user data")
+
+            user = self.get_user(user_data)
             return user, None
-        except (ValueError, User.DoesNotExist):
+        except ValueError:
+            return None
+        except Exception as err:
+            logger.exception(err)
             return None
