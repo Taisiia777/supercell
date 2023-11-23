db908a8cde8e97d44373b43f5790ef2f640d5c41
Satyuel <stockmann89@mail.ru>
2023-11-24 02:50:08 +0300

    Fix Product-DavDamer relation

--- backend/shop/catalogue/models.py
+++ backend/shop/catalogue/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.db.models import ForeignKey
 from oscar.apps.catalogue.abstract_models import AbstractProduct
-from oscar.core.loading import get_model
 
-Seller = get_model("partner", "Seller")
+from core.models import DavDamer
 
 
 class Product(AbstractProduct):
-    seller = ForeignKey(Seller, on_delete=models.PROTECT, null=True)
+    davdamer = ForeignKey(DavDamer, on_delete=models.PROTECT, null=True)
 
 
 from oscar.apps.catalogue.models import *  # noqa
