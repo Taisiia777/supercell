14375fab5102ee9c072a70f5f2cd2012840ed2c3
Satyuel <stockmann89@mail.ru>
2023-11-24 03:41:48 +0300

    Fix oscarapi_post_checkout signal receiver

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,10 +1,7 @@
 from django.dispatch import receiver
 from oscarapi.signals import oscarapi_post_checkout
-from oscarapi.utils.loading import get_api_class
-
-CheckoutView = get_api_class("views.checkout", "CheckoutView")
 
 
-@receiver(oscarapi_post_checkout, sender=CheckoutView)
+@receiver(oscarapi_post_checkout)
 def created_api_callback_handler(sender, order, user, **kwargs):
     print("Caught oscarapi_post_checkout signal")
