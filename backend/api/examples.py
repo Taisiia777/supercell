fc5f40dd9db21bb9127365d74063e3d522ec981e
Satyuel <stockmann89@mail.ru>
2023-12-01 23:59:31 +0300

    Add total value for example checkout request

--- backend/api/examples.py
+++ backend/api/examples.py
@@ -2,6 +2,7 @@ CheckoutExample = {
     "products": [
         {"product_id": 3, "quantity": 1},
     ],
+    "total": 1500.99,
     "shipping_address": {
         "first_name": "Иван",
         "last_name": "Иванов",
