b11049f35875fb7a81e6ecdc71c562b944b4776a
Satyuel <stockmann89@mail.ru>
2023-12-30 22:51:06 +0300

    Fix JWT authentication fallback

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -179,8 +179,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "api.authentication.WebAppAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 HAYSTACK_CONNECTIONS = {
