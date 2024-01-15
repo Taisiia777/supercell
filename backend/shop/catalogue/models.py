a16e1c4b30e5e864a9a370232cffb4ae2e1c5300
Satyuel <stockmann89@mail.ru>
2024-01-16 01:03:00 +0300

    Remove unnecessary product.davdamer field

--- backend/shop/catalogue/models.py
+++ backend/shop/catalogue/models.py
@@ -2,11 +2,8 @@ from django.db import models
 from django.db.models import ForeignKey
 from oscar.apps.catalogue.abstract_models import AbstractProduct
 
-from core.models import DavDamer
-
 
 class Product(AbstractProduct):
-    davdamer = ForeignKey(DavDamer, on_delete=models.PROTECT, null=True)
     seller = ForeignKey(
         "partner.Seller", on_delete=models.PROTECT, null=True, related_name="products"
     )
