f419ccfba7e64a48fb8361b4ada54893f770bfa2
Satyuel <stockmann89@mail.ru>
2024-01-19 15:12:03 +0300

    Setup backend private_urls

---
+++ backend/api/spectacular_hooks.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+
+def filter_endpoints(endpoints):
+    filtered = []
+    for path, path_regex, method, callback in endpoints:
+        if settings.DEBUG is False and path.startswith("/private_api/"):
+            continue
+        filtered.append((path, path_regex, method, callback))
+    return filtered
