23defd7368c0eb2f87d04f6e4b8e8d73ba2e06bd
Satyuel <stockmann89@mail.ru>
2024-01-22 00:05:31 +0300

    Create customer_bot skeleton

---
+++ customer_bot/src/handlers/__init__.py
@@ -0,0 +1,37 @@
+import logging
+
+from aiogram import Router
+from aiogram.filters import ExceptionTypeFilter, Command
+from aiogram.types import ErrorEvent, Message
+
+from .callbacks import router as callbacks_router
+from .commands import router as commands_router
+from ..filters import PrivateChatFilter
+
+logger = logging.getLogger(__name__)
+
+router = Router()
+
+router.message.filter(PrivateChatFilter())
+router.callback_query.filter(PrivateChatFilter())
+router.include_routers(commands_router, callbacks_router)
+
+
+@router.message(Command("get_id"))
+async def get_chat_id_handler(message: Message):
+    await message.answer(str(message.chat.id))
+
+
+@router.error(ExceptionTypeFilter(Exception))
+async def error_handler(event: ErrorEvent):
+    logger.exception(event.exception)
+
+    try:
+        chat_id = event.update.event.chat.id
+    except Exception:
+        chat_id = event.update.event.from_user.id
+
+    await event.update.bot.send_message(
+        chat_id,
+        "Произошла ошибка бота, повторите попытку позже или введите /start",
+    )
---
+++ customer_bot/src/utils.py
@@ -0,0 +1,5 @@
+import datetime
+
+
+def format_dt(dt: datetime.datetime):
+    return dt.strftime("%d.%m.%Y %H:%M:%S")
