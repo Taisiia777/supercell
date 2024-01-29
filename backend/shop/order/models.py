fd871f71b4aae0db5c23ea458404b2848779eaa3
Satyuel <stockmann89@mail.ru>
2024-01-29 21:26:32 +0300

    Fix calculating total_orders on creating seller

--- backend/shop/order/models.py
+++ backend/shop/order/models.py
@@ -8,7 +8,11 @@ class Order(AbstractOrder):
     custom_field = models.CharField(max_length=255, blank=True, null=True)
     status = models.CharField(max_length=100, blank=True, choices=OrderStatus.choices)
     seller = models.ForeignKey(
-        "partner.Seller", on_delete=models.CASCADE, blank=True, null=True
+        "partner.Partner",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="orders",
     )
     updated_dt = models.DateTimeField(auto_now=True, blank=True, null=True)
 
