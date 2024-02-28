b30e29ae2b0dd1e2e04561288d90c79f42587432
Satyuel <stockmann89@mail.ru>
2024-02-28 14:58:01 +0300

    Update customer bot hello text

--- customer_bot/src/handlers/commands.py
+++ customer_bot/src/handlers/commands.py
@@ -5,10 +5,33 @@ from create_bot import settings
 
 router = Router()
 
+HELLO_TEXT = """Добро пожаловать - приложение «ДОСТАВКА ФУДСИТИ»!
+
+Все самое свежее и натуральное с легендарного рынка! 
+
+👉 Бесплатная доставка
+👉 Низкие цены
+👉 Гарантия качества
+
+Продукты со всего мира 🌍"""
+webapp_button = None
+if settings.CUSTOMER_WEBAPP_URL:
+    webapp_button = types.InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text="Магазин",
+                    web_app=types.WebAppInfo(url=str(settings.CUSTOMER_WEBAPP_URL)),
+                )
+            ]
+        ]
+    )
+
 
 @router.message(Command("start"))
 async def start_command(message: types.Message):
-    await message.answer("Добро пожаловать!")
+    await message.answer(HELLO_TEXT, reply_markup=webapp_button)
+
     if settings.CUSTOMER_WEBAPP_URL:
         await message.bot.set_chat_menu_button(
             message.from_user.id,
