68411079d1196d0d004d694b6a4076466037f8e3
Satyuel <stockmann89@mail.ru>
2024-03-17 13:49:34 +0300

    Update requests examples

--- backend/api/examples.py
+++ backend/api/examples.py
@@ -1,19 +1,8 @@
 CheckoutExample = {
     "products": [
-        {"product_id": 3, "quantity": 1},
+        {"product_id": 3, "quantity": 1, "account_id": "email@example.com"},
     ],
     "total": 1500.99,
-    "shipping_address": {
-        "first_name": "Иван",
-        "last_name": "Иванов",
-        "line1": "ул. Пушкина, д. 1",
-        "state": "Москва",
-        "district": "ЖК «Скандинавия»",
-        "phone_number": "+7 (999) 999-99-99",
-        "notes": "Доставить вечером",
-        "date": "2024-03-17",
-        "time": "08:00-20:00",
-    },
 }
 
 CreateProductExample = {
@@ -23,13 +12,7 @@ CreateProductExample = {
     "old_price": 1999,
     "measurement": "шт.",
     "is_public": True,
-    "country": "Россия",
-    "is_vegan": True,
-    "is_sugar_free": False,
-    "is_gluten_free": False,
-    "is_dietary": False,
     "categories": ["Одежда"],
-    "attributes": [{"code": "size", "value": "XS"}],
     "login_type": "EMAIL_CODE",
 }
 
@@ -40,15 +23,8 @@ UpdateProductExample = {
     "old_price": 1999,
     "measurement": "шт.",
     "is_public": True,
-    "country": "Россия",
-    "is_vegan": True,
-    "is_sugar_free": False,
-    "is_gluten_free": False,
-    "is_dietary": False,
     "categories": ["Одежда"],
-    "attributes": [{"code": "size", "value": "XXL"}],
     "uploaded_images": [],
     "deleted_images": [1],
-    "new_seller_id": 1,
     "login_type": "LINK",
 }
